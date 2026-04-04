60063b39c5afca110ab82ab55add847f53eb95a4
Glen Coden <info@simonmeyer.de>
2026-04-04 18:18:34 +0200

    add application constants and variables

---
+++ src/variables.ts
@@ -0,0 +1,34 @@
+export const appVersion = '2.0.0';
+
+export const validGenres = ['Jazz', 'Pop', 'Rock', 'Electronic', 'HipHop', 'World'];
+export const minNumRenderedTiles = 3;
+
+export const playlistFilterKey = 'berta.berlin';
+export const maxVideoListLength = Infinity;
+export const genreQuotaPercentage = 10;
+
+export const minNumUnseenVideos = 50;
+export const genreListStaleTime = 1000 * 60 * 60 * 24 * 7;
+
+export const defaultTileWidth = 1280;
+export const minDeviceWidth = 600;
+export const fullDeviceWidth = 1440;
+export const maxMobileWidth = 900;
+export const mobileContentMargin = 14;
+
+export const navigationMargin = 2;
+export const navigationZIndex = 1000;
+export const sidebarWidth = 80;
+
+export const laneLeft = 110;
+export const laneTop = 110;
+export const laneTileOffset = 50;
+export const hideTileSafetyOffset = 50;
+
+export const progressBarWidth = 274;
+
+export const controlsMargin = 2;
+export const controlsOverlayWidth = 38;
+
+export const laneTileSlideInDelay = 200;
+export const laneTileAnimationOffset = 50;
